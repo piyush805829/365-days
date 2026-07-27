36a8394e2fc8f2e0d194d540bfe8230aac8d8a0d
Piyush <sainipiyush5924@gmail.com>
2026-07-27 23:21:36 +0530

    Day 1: Variables and cin/cout

---
+++ c++/day-1/variables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+string playername;
+int health, level;
+
+cout << "Enter Player Name : ";
+cin >> playername;
+
+cout << "Enter Player Health : ";
+cin >> health;
+
+cout << "Enter Player Level : ";
+cin >> level;
+
+cout << "Player Name is : " << playername << endl;
+cout << "Players Health is : " << health << endl;
+cout << "Players Level is : " << level << endl;
+
+return 0;
+
+}
+ 
