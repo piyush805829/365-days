7db0e6a3bc88819f0ddb5d6616c0f743ebfa12d0
Piyush <sainipiyush5924@gmail.com>
2026-08-03 12:35:03 +0530

    day 2 : conditionls & loops

---
+++ c++/day2/loops.cpp
@@ -0,0 +1,58 @@
+/*
+
+// FOR LOOP
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+
+*/    
+
+
+// while loop
+
+/*
+
+int main()
+{
+    int i = 1;
+
+    while (i <= 5)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
+
+*/
+
+// do while loop
+
+/*
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+        cout << i << endl;
+        i++;
+    }
+    while (i <= 5);
+
+    return 0;
+}
+
+*/
+
